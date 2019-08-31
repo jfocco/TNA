5a0c9d26aab9514f56a5b0fb3050f8cd9ffb4f95
Juan Focco <juanfocco@hotmail.com>
2019-08-31 18:42:37 -0300

    Updates

---
+++ precache-manifest.650588de1b803f7d0d1619075c21fa6c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa9cebb6f5c270db929fc8ca8f46c59",
+    "url": "/TNA/index.html"
+  },
+  {
+    "revision": "5012aaeaba4590f7e3b2",
+    "url": "/TNA/static/css/2.84af191b.chunk.css"
+  },
+  {
+    "revision": "b9acdb03d5f5dbe93c07",
+    "url": "/TNA/static/css/main.3d937507.chunk.css"
+  },
+  {
+    "revision": "5012aaeaba4590f7e3b2",
+    "url": "/TNA/static/js/2.91a01539.chunk.js"
+  },
+  {
+    "revision": "b9acdb03d5f5dbe93c07",
+    "url": "/TNA/static/js/main.12c16333.chunk.js"
+  },
+  {
+    "revision": "217772c150e03242ee3a",
+    "url": "/TNA/static/js/runtime~main.f42caf64.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/TNA/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/TNA/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/TNA/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/TNA/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/TNA/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
